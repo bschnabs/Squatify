87fe64a22701c40754995e5a7fec331cba4c9398
Brian Ellis <bellis@saberlogic.com>
2016-02-21 20:10:44 -0500

    Added jade index and includes

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,24 @@
 ///<reference path='./typings/main.d.ts' />
 import express = require('express');
 import path = require('path');
+import jade = require('jade');
+
 var port: number = process.env.PORT || 3000;
 var app = express();
 
 app.use('/node_modules', express.static(path.resolve(__dirname, '../node_modules')));
 app.use('/app', express.static(path.resolve(__dirname, '../client/app')));
 
-var renderIndex = (req: express.Request, res: express.Response) => {
-    res.sendFile(path.resolve(__dirname, '../client/index.test.html'));
-}
+app.set('view engine', 'jade');
 
-app.get('/*', renderIndex);
+//var renderIndex = (req: express.Request, res: express.Response) => {
+//    res.sendFile(path.resolve(__dirname, '../client/index.test.html'));
+//}
+//app.get('/*', renderIndex);
+
+app.get('/', function (req, res) {
+  res.render('../client/index');
+});
 
 var server = app.listen(port, function() {
     var host = server.address().address;
